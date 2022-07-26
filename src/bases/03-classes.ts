f52caf1e1dc9fb88663978e96e2749b475151666
Alejo Boga <alejoboga20@gmail.com>
2022-07-26 14:24:15 -0300

    Getters

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -13,6 +13,24 @@ Long way
 //best way
 export class Pokemon {
 	constructor(public readonly id: number, public name: string) {}
+
+	//getter
+	get imageUrl(): string {
+		return `https://pokemon.com/${this.id}.jpg`;
+	}
+
+	//methods: functions with access to properties and other methods
+	public scream() {
+		this.speak();
+		console.log(`${this.name.toUpperCase()}!!!`);
+	}
+
+	//private methods are only accesible from inside the class
+	private speak() {
+		console.log(`Hello, my name is ${this.name}`);
+	}
 }
 
-export const charmander = new Pokemon(1, 'charmander');
+export const charmander = new Pokemon(4, 'charmander');
+console.log(charmander);
+charmander.scream();
